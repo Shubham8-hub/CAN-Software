3d5ad6ed39921a91662a49b24c32ede0703e35da
Shubham Samal <shubham.saamal@gmail.com>
2024-02-02 22:43:05 +0530

    Added CAN, J1939, LIN and Tools Tab

--- CAN-software/CAN-software.Core/obj/Debug/netstandard2.0/CAN-software.Core.AssemblyInfo.cs
+++ CAN-software/CAN-software.Core/obj/Debug/netstandard2.0/CAN-software.Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CAN-software.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4005ace4a0a4c42737844d9037f54d16f072834")]
 [assembly: System.Reflection.AssemblyProductAttribute("CAN-software.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CAN-software.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- CAN-software/CAN-software/Properties/Resources.Designer.cs
+++ CAN-software/CAN-software/Properties/Resources.Designer.cs
@@ -10,7 +10,8 @@
 
 namespace CAN_software.Properties {
     using System;
-
+    
+    
     /// <summary>
     ///   A strongly-typed resource class, for looking up localized strings, etc.
     /// </summary>
@@ -18,19 +19,19 @@ namespace CAN_software.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class Resources {
-
+        
         private static global::System.Resources.ResourceManager resourceMan;
-
+        
         private static global::System.Globalization.CultureInfo resourceCulture;
-
+        
         [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
         internal Resources() {
         }
-
+        
         /// <summary>
         ///   Returns the cached ResourceManager instance used by this class.
         /// </summary>
@@ -44,7 +45,7 @@ namespace CAN_software.Properties {
                 return resourceMan;
             }
         }
-
+        
         /// <summary>
         ///   Overrides the current thread's CurrentUICulture property for all
         ///   resource lookups using this strongly typed resource class.
@@ -58,205 +59,337 @@ namespace CAN_software.Properties {
                 resourceCulture = value;
             }
         }
-
+        
         /// <summary>
-        ///   Looks up a localized string similar to ts.ItemName.
+        ///   Looks up a localized string similar to CAN_software.
         /// </summary>
         public static string AppDisplayName {
             get {
                 return ResourceManager.GetString("AppDisplayName", resourceCulture);
             }
         }
-
+        
         /// <summary>
-        ///   Looks up a localized string similar to ShellPageRibbonHomeGroupCommon1Header.
+        ///   Looks up a localized string similar to ContentGridpg.
+        /// </summary>
+        public static string ContentGridpgPageTitle {
+            get {
+                return ResourceManager.GetString("ContentGridpgPageTitle", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to DisplayCanGridPg.
+        /// </summary>
+        public static string DisplayCanGridPgPageTitle {
+            get {
+                return ResourceManager.GetString("DisplayCanGridPgPageTitle", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to HomeRibbonpg.
+        /// </summary>
+        public static string HomeRibbonpgPageTitle {
+            get {
+                return ResourceManager.GetString("HomeRibbonpgPageTitle", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Sample Orders.
+        /// </summary>
+        public static string ListDetailspgListDescription {
+            get {
+                return ResourceManager.GetString("ListDetailspgListDescription", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to ListDetailspg.
+        /// </summary>
+        public static string ListDetailspgPageTitle {
+            get {
+                return ResourceManager.GetString("ListDetailspgPageTitle", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Settings page placeholder text.  Your app description goes here..
+        /// </summary>
+        public static string SettingsPageAboutText {
+            get {
+                return ResourceManager.GetString("SettingsPageAboutText", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to About this application.
+        /// </summary>
+        public static string SettingsPageAboutTitle {
+            get {
+                return ResourceManager.GetString("SettingsPageAboutTitle", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Choose Theme.
+        /// </summary>
+        public static string SettingsPageChooseThemeText {
+            get {
+                return ResourceManager.GetString("SettingsPageChooseThemeText", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Personalization.
+        /// </summary>
+        public static string SettingsPagePersonalizationTitle {
+            get {
+                return ResourceManager.GetString("SettingsPagePersonalizationTitle", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Privacy Statement.
+        /// </summary>
+        public static string SettingsPagePrivacyStatementText {
+            get {
+                return ResourceManager.GetString("SettingsPagePrivacyStatementText", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Dark.
+        /// </summary>
+        public static string SettingsPageRadioButtonDarkTheme {
+            get {
+                return ResourceManager.GetString("SettingsPageRadioButtonDarkTheme", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Light.
+        /// </summary>
+        public static string SettingsPageRadioButtonLightTheme {
+            get {
+                return ResourceManager.GetString("SettingsPageRadioButtonLightTheme", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Default.
+        /// </summary>
+        public static string SettingsPageRadioButtonWindowsDefaultTheme {
+            get {
+                return ResourceManager.GetString("SettingsPageRadioButtonWindowsDefaultTheme", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Settings.
+        /// </summary>
+        public static string SettingsPageTitle {
+            get {
+                return ResourceManager.GetString("SettingsPageTitle", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Settings.
+        /// </summary>
+        public static string ShellPageRibbonBackstageTabSettingsHeader {
+            get {
+                return ResourceManager.GetString("ShellPageRibbonBackstageTabSettingsHeader", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Hardware Configuration.
+        /// </summary>
+        public static string ShellPageRibbonCANGroupCommon1Header {
+            get {
+                return ResourceManager.GetString("ShellPageRibbonCANGroupCommon1Header", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Database.
+        /// </summary>
+        public static string ShellPageRibbonCANGroupCommon2Header {
+            get {
+                return ResourceManager.GetString("ShellPageRibbonCANGroupCommon2Header", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Measurement Window.
+        /// </summary>
+        public static string ShellPageRibbonCANGroupCommon3Header {
+            get {
+                return ResourceManager.GetString("ShellPageRibbonCANGroupCommon3Header", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Simulation Window.
+        /// </summary>
+        public static string ShellPageRibbonCANGroupCommon4Header {
+            get {
+                return ResourceManager.GetString("ShellPageRibbonCANGroupCommon4Header", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Files.
         /// </summary>
         public static string ShellPageRibbonHomeGroupCommon1Header {
             get {
                 return ResourceManager.GetString("ShellPageRibbonHomeGroupCommon1Header", resourceCulture);
             }
         }
-
+        
         /// <summary>
-        ///   Looks up a localized string similar to ShellPageRibbonHomeGroupCommon2Header.
+        ///   Looks up a localized string similar to Common Group 2.
         /// </summary>
         public static string ShellPageRibbonHomeGroupCommon2Header {
             get {
                 return ResourceManager.GetString("ShellPageRibbonHomeGroupCommon2Header", resourceCulture);
             }
         }
-
+        
         /// <summary>
-        ///   Looks up a localized string similar to ShellPageRibbonTabHomeHeader.
+        ///   Looks up a localized string similar to Activate.
+        /// </summary>
+        public static string ShellPageRibbonJ1939GroupCommon1Header {
+            get {
+                return ResourceManager.GetString("ShellPageRibbonJ1939GroupCommon1Header", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Database.
+        /// </summary>
+        public static string ShellPageRibbonJ1939GroupCommon2Header {
+            get {
+                return ResourceManager.GetString("ShellPageRibbonJ1939GroupCommon2Header", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to J1939 Setting.
+        /// </summary>
+        public static string ShellPageRibbonJ1939GroupCommon3Header {
+            get {
+                return ResourceManager.GetString("ShellPageRibbonJ1939GroupCommon3Header", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Measurement Window.
+        /// </summary>
+        public static string ShellPageRibbonJ1939GroupCommon4Header {
+            get {
+                return ResourceManager.GetString("ShellPageRibbonJ1939GroupCommon4Header", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Simulation Window.
+        /// </summary>
+        public static string ShellPageRibbonJ1939GroupCommon5Header {
+            get {
+                return ResourceManager.GetString("ShellPageRibbonJ1939GroupCommon5Header", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Hardware Configuration.
+        /// </summary>
+        public static string ShellPageRibbonLINGroupCommon1Header {
+            get {
+                return ResourceManager.GetString("ShellPageRibbonLINGroupCommon1Header", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Measurement Window.
+        /// </summary>
+        public static string ShellPageRibbonLINGroupCommon2Header {
+            get {
+                return ResourceManager.GetString("ShellPageRibbonLINGroupCommon2Header", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Simulation Window.
+        /// </summary>
+        public static string ShellPageRibbonLINGroupCommon3Header {
+            get {
+                return ResourceManager.GetString("ShellPageRibbonLINGroupCommon3Header", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to CAN.
+        /// </summary>
+        public static string ShellPageRibbonTabCANHeader {
+            get {
+                return ResourceManager.GetString("ShellPageRibbonTabCANHeader", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Home.
         /// </summary>
         public static string ShellPageRibbonTabHomeHeader {
             get {
                 return ResourceManager.GetString("ShellPageRibbonTabHomeHeader", resourceCulture);
             }
         }
-
+        
         /// <summary>
-        ///   Looks up a localized string similar to HomeRibbonpg.
+        ///   Looks up a localized string similar to J1939.
         /// </summary>
-        public static string HomeRibbonpgPageTitle
-        {
-            get
-            {
-                return ResourceManager.GetString("HomeRibbonpgPageTitle", resourceCulture);
+        public static string ShellPageRibbonTabJ1939Header {
+            get {
+                return ResourceManager.GetString("ShellPageRibbonTabJ1939Header", resourceCulture);
             }
         }
-
+        
         /// <summary>
-        ///   Looks up a localized string similar to DisplayCanGridPg.
+        ///   Looks up a localized string similar to LIN.
         /// </summary>
-        public static string DisplayCanGridPgPageTitle
-        {
-            get
-            {
-                return ResourceManager.GetString("DisplayCanGridPgPageTitle", resourceCulture);
+        public static string ShellPageRibbonTabLINHeader {
+            get {
+                return ResourceManager.GetString("ShellPageRibbonTabLINHeader", resourceCulture);
             }
         }
-
+        
         /// <summary>
-        ///   Looks up a localized string similar to Sample Orders.
+        ///   Looks up a localized string similar to Tools.
         /// </summary>
-        public static string ListDetailspgListDescription
-        {
-            get
-            {
-                return ResourceManager.GetString("ListDetailspgListDescription", resourceCulture);
+        public static string ShellPageRibbonTabToolsHeader {
+            get {
+                return ResourceManager.GetString("ShellPageRibbonTabToolsHeader", resourceCulture);
             }
         }
-
+        
         /// <summary>
-        ///   Looks up a localized string similar to ListDetailspg.
+        ///   Looks up a localized string similar to Format Converter.
         /// </summary>
-        public static string ListDetailspgPageTitle
-        {
-            get
-            {
-                return ResourceManager.GetString("ListDetailspgPageTitle", resourceCulture);
+        public static string ShellPageRibbonToolsGroupCommon1Header {
+            get {
+                return ResourceManager.GetString("ShellPageRibbonToolsGroupCommon1Header", resourceCulture);
             }
         }
-
+        
         /// <summary>
-        ///   Looks up a localized string similar to ContentGridpg.
+        ///   Looks up a localized string similar to Editor.
         /// </summary>
-        public static string ContentGridpgPageTitle
-        {
-            get
-            {
-                return ResourceManager.GetString("ContentGridpgPageTitle", resourceCulture);
-            }
-        }
-
-        /// <summary>
-        ///   Looks up a localized string similar to Settings page placeholder text.  Your app description goes here..
-        /// </summary>
-        public static string SettingsPageAboutText
-        {
-            get
-            {
-                return ResourceManager.GetString("SettingsPageAboutText", resourceCulture);
-            }
-        }
-
-        /// <summary>
-        ///   Looks up a localized string similar to About this application.
-        /// </summary>
-        public static string SettingsPageAboutTitle
-        {
-            get
-            {
-                return ResourceManager.GetString("SettingsPageAboutTitle", resourceCulture);
-            }
-        }
-
-        /// <summary>
-        ///   Looks up a localized string similar to Choose Theme.
-        /// </summary>
-        public static string SettingsPageChooseThemeText
-        {
-            get
-            {
-                return ResourceManager.GetString("SettingsPageChooseThemeText", resourceCulture);
-            }
-        }
-
-        /// <summary>
-        ///   Looks up a localized string similar to Personalization.
-        /// </summary>
-        public static string SettingsPagePersonalizationTitle
-        {
-            get
-            {
-                return ResourceManager.GetString("SettingsPagePersonalizationTitle", resourceCulture);
-            }
-        }
-
-        /// <summary>
-        ///   Looks up a localized string similar to Privacy Statement.
-        /// </summary>
-        public static string SettingsPagePrivacyStatementText
-        {
-            get
-            {
-                return ResourceManager.GetString("SettingsPagePrivacyStatementText", resourceCulture);
-            }
-        }
-
-        /// <summary>
-        ///   Looks up a localized string similar to Dark.
-        /// </summary>
-        public static string SettingsPageRadioButtonDarkTheme
-        {
-            get
-            {
-                return ResourceManager.GetString("SettingsPageRadioButtonDarkTheme", resourceCulture);
-            }
-        }
-
-        /// <summary>
-        ///   Looks up a localized string similar to Light.
-        /// </summary>
-        public static string SettingsPageRadioButtonLightTheme
-        {
-            get
-            {
-                return ResourceManager.GetString("SettingsPageRadioButtonLightTheme", resourceCulture);
-            }
-        }
-
-        /// <summary>
-        ///   Looks up a localized string similar to Default.
-        /// </summary>
-        public static string SettingsPageRadioButtonWindowsDefaultTheme
-        {
-            get
-            {
-                return ResourceManager.GetString("SettingsPageRadioButtonWindowsDefaultTheme", resourceCulture);
-            }
-        }
-
-        /// <summary>
-        ///   Looks up a localized string similar to Settings.
-        /// </summary>
-        public static string SettingsPageTitle
-        {
-            get
-            {
-                return ResourceManager.GetString("SettingsPageTitle", resourceCulture);
-            }
-        }
-
-        /// <summary>
-        ///   Looks up a localized string similar to ShellPageRibbonBackstageTabSettingsHeader.
-        /// </summary>
-        public static string ShellPageRibbonBackstageTabSettingsHeader
-        {
-            get
-            {
-                return ResourceManager.GetString("ShellPageRibbonBackstageTabSettingsHeader", resourceCulture);
+        public static string ShellPageRibbonToolsGroupCommon2Header {
+            get {
+                return ResourceManager.GetString("ShellPageRibbonToolsGroupCommon2Header", resourceCulture);
             }
         }
     }
